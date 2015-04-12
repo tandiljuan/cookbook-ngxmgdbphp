f12c0ac93be19447575aded3a9809d47c97975bc
Juan Manuel Lopez <tandiljuan@gmail.com>
2015-04-11 21:27:29 -0300

    Set version 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Juan Manuel Lopez"
 maintainer_email ""
 license          "Apache 2.0"
 description      "Setup a Nginx - MongoDB - PHP environment"
-version          "0.0.1"
+version          "0.0.2"
 
 depends "cookbook-core",    "~> 0.0.2"
 depends "mongodb",          "~> 0.16.1"
